e92acb3e8073704aa68c2f2a59cb22a960d805a4
frederikdupont <cph-fd101@cphbusiness.dk>
2024-12-11 10:14:58 +0100

    fred lidt sjovt fra nederste hylde

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import RootLayout from './layouts/RootLayout';
+import Home from './pages/Home'; // Ensure correct file path and naming
+import Error404Page from './pages/Error404Page';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} /> 
+      <Route path="*" element={<Error404Page
+       />} />
+    </Route>
+  )
+);
 
 function App() {
- 
-
-  return (
-    <>
- 
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
