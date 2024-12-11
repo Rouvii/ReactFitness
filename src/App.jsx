6437ec7bbbf38c820b6378d33ab4fa427b2f2e1e
Rouvii <115996187+Rouvii@users.noreply.github.com>
2024-12-11 10:39:09 +0100

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-
-  
-import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import RootLayout from './layouts/RootLayout';
-import Home from './pages/Home'; // Ensure correct file path and naming
-import Error404Page from './pages/Error404Page';
-import LoginPage from "./pages/LoginPage"
-
-
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import Home from "./pages/Home"; // Ensure correct file path and naming
+import Error404Page from "./pages/Error404Page";
+import LoginPage from "./pages/LoginPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} /> 
-      <Route path="*" element={<Error404Page
-       />} />
+      <Route index element={<Home />} />
+      <Route path="*" element={<Error404Page />} />
+      <Route path="/loginPage" element={<LoginPage />} />
     </Route>
-
-  return (
-    <>
-  <LoginPage />
-    <RouterProvider router={router} />;
-    </>
-
   )
 );
 
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />;
+    </>
+  );
+}
 
-
-export default App
+export default App;
